a14b3edce7f8f18ffb07d9f389f5f009db5eb253
Gio Valdes <Geovanny.Valdes@walmart.com>
2023-06-13 09:49:48 -0500

    Initial Commit

---
+++ swiftUITest/swiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUITestApp.swift
+//  swiftUITest
+//
+//  Created by Geovanny Valdes on 13/06/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
